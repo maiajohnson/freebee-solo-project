d776048a5e6f37ef37cedcda4291f49da39f893b
Maia Johnson <maiaj1306@gmail.com>
2022-11-14 12:55:29 -0600

    added in sms alert capability

---
+++ send_sms.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'I love getting alerts!!',
+     from: '+15139514646',
+     to: '+19528184985'
+   })
+  .then(message => console.log(message.sid));
